eee4d42b8c557ebb0d6bdbe55024232fc42f2956
유하람 <w2036@e-mirim.hs.kr>
2021-07-19 10:24:30 +0900

    Change activity_main And Add ThirdActivity

--- app/src/main/java/kr/hs/emirim/w2036/project_10_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2036/project_10_1/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-
         }
     };
 }
