e81f092a75aae6cbddc4f8d8acb89b47b1921e62
유하람 <w2036@e-mirim.hs.kr>
2021-07-19 10:33:37 +0900

    Completion ThirdActivity.java

--- app/src/main/java/kr/hs/emirim/w2036/project_10_1/ThirdActivity.java
+++ app/src/main/java/kr/hs/emirim/w2036/project_10_1/ThirdActivity.java
@@ -3,6 +3,8 @@ package kr.hs.emirim.w2036.project_10_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -11,6 +13,14 @@ public class ThirdActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
 
-
+        Button btnThirdClose = findViewById(R.id.btn_third_close);
+        btnThirdClose.setOnClickListener(btnThirdCloseLitener);
     }
+
+    View.OnClickListener btnThirdCloseLitener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
 }
